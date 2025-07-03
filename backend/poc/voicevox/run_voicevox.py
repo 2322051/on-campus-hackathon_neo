0f8501e15c2efa217bbfbd7057691b467f9075e5
Takahashi-Kaisei <130371620+Takahashi-Kaisei@users.noreply.github.com>
2025-07-03 21:10:13 +0900

    fix: speakerの設定を変更し、音声合成の出力パスを動的に生成するように修正

--- backend/poc/voicevox/run_voicevox.py
+++ backend/poc/voicevox/run_voicevox.py
@@ -4,7 +4,13 @@ from VoicevoxEngine import VoicevoxClient
 text = """
 この論文は、大規模言語モデル（LLM）が新しい知識を継続的に学習し、人間のように記憶する能力を高めるための新しいフレームワーク「HippoRAG 2」を提案しています。現在のRAG（Retrieval-Augmented Generation：検索拡張生成）システムが持つ課題、特にベクトル検索に依存することによる人間の長期記憶のような動的で相互接続された性質を模倣できない点を克服しようとしています。
 """
-speaker = 1
+speaker = 2
 
-model = VoicevoxClient(text=text, speaker=speaker)
-model.synthesize_voice(output_path="../../data/voices/002_HippoRAG2解説.wav")
+ID = "003"
+
+model = VoicevoxClient()
+model.synthesize_voice(
+    text=text,
+    speaker=speaker,
+    output_path=f"../../data/voices/{ID}_{speaker}.wav"
+)
